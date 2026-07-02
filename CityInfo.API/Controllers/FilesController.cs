ef16806aeaddf9ec4b5c8ba3436ac104a5c29a6b
Nathan Barbosa Dos Santos <n.barbosa.dos.santos@avanade.com>
2026-07-02 17:49:47 -0300

    feat: adding file creation method

--- CityInfo.API/Controllers/FilesController.cs
+++ CityInfo.API/Controllers/FilesController.cs
@@ -33,5 +33,23 @@ namespace CityInfo.API.Controllers
             var bytes = System.IO.File.ReadAllBytes(pathToFile);
             return File(bytes, contentType, Path.GetFileName(pathToFile));
         }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateFile(IFormFile file)
+        {
+            if(file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+            {
+                return BadRequest("No file or an invalid one has been inputted.");
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("Your file has been uploaded succesfully!");
+        }
     }
 }
