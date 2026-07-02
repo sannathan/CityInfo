470032789a102b94289c4e334e90ef386f6462f2
Nathan Barbosa Dos Santos <n.barbosa.dos.santos@avanade.com>
2026-07-02 15:29:21 -0300

    feat: adding Data Annotations to validate incoming data

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -46,6 +46,7 @@ namespace CityInfo.API.Controllers
            int cityId,
            PointOfInterestForCreationDto pointOfInterest)
         {
+
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
             if(city == null)
