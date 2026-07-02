0d53a5e2838160e5cfe6b02ca2b66fe6c50b6a3e
Nathan Barbosa Dos Santos <n.barbosa.dos.santos@avanade.com>
2026-07-02 14:43:24 -0300

    feat: adding file controller

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,7 @@ builder.Services.AddControllers(
 //})
 
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
