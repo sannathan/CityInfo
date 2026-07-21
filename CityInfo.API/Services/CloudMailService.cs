582ca476ff470fd9733332f7ddbf2697e8a97d15
Nathan Barbosa Dos Santos <n.barbosa.dos.santos@avanade.com>
2026-07-21 10:22:24 -0300

    feat: adding mail services

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -1,21 +1,19 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace CityInfo.API.Services
+﻿namespace CityInfo.API.Services
 {
-    public class LocalMailService : IMailService
+    public class CloudMailService : IMailService
     {
         private string _mailTo = string.Empty;
         private string _mailFrom = string.Empty;
-        public LocalMailService(IConfiguration configuration)
+
+        public CloudMailService(IConfiguration configuration)
         {
             _mailTo = configuration["mailSettings:mailToAddress"];
             _mailFrom = configuration["mailSettings:mailFromAddress"];
-            
         }
 
         public void Send(string subject, string message)
         {
-            Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with {nameof(LocalMailService)}.");
+            Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with {nameof(CloudMailService)}.");
             Console.WriteLine($"Subject: {subject}");
             Console.WriteLine($"Message: {message}");
         }
