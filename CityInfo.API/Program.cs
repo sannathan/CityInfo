6fda16a9f2eabf6a6b9b61b05f077a07f11f176a
Nathan Barbosa Dos Santos <n.barbosa.dos.santos@avanade.com>
2026-07-21 10:24:10 -0300

    feat: adding environment dependent service register

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,3 +1,4 @@
+using CityInfo.API;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
 using Serilog;
@@ -36,7 +37,13 @@ builder.Services.AddProblemDetails();
 
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
-builder.Services.AddTransient<LocalMailService>();
+builder.Services.AddSingleton<CitiesDataStore>();
+
+#if DEBUG
+builder.Services.AddTransient<IMailService, LocalMailService>();
+#else
+builder.Services.AddTransient<IMailService, CloudMailService>();
+#endif
 
 var app = builder.Build();
 
